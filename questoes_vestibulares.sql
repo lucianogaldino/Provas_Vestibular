5ffd303ae4a3e5f05ae02b12e86dab27ac209e94
Luciano Galdino <69773452+lucianogaldino@users.noreply.github.com>
2020-10-03 11:28:16 -0700

    Add files via upload

---
+++ questoes_vestibulares.sql
@@ -0,0 +1,162 @@
+create database vestibular
+default character set utf8 
+default collate utf8_general_ci;
+
+use vestibular;
+
+create table questoes (
+id int not null auto_increment,
+disciplina varchar(30), 
+ano varchar(30), 
+vestibular varchar (30),
+conteúdo varchar(30),
+primary key (id) 
+) default charset = utf8;
+
+alter table questoes
+add column questao decimal(5);
+
+alter table questoes
+change column vestibular fase varchar(20);
+
+alter table questoes
+add column assunto varchar(10) after fase;
+
+alter table questoes
+change column conteúdo conteudo varchar(30);
+
+alter table questoes
+add column vestibular varchar(20) after id;
+
+alter table questoes
+modify column assunto varchar(20);
+
+insert into questoes values
+(Default, 'fuvest', 'Física', '2016','primeira', 'mecânica', 'energia mecânica', '24'),
+(Default, 'fuvest','Física', '2016','primeira', 'eletrostática', 'conservação da carga elétrica', '25'),
+(Default, 'fuvest','Física', '2016','primeira', 'oscilação', 'pêndulo', '26'),
+(Default, 'fuvest','Física', '2016','primeira', 'eletrodinâmica', 'circuito', '27'),
+(Default, 'fuvest','Física', '2016','primeira', 'gravitação', 'aceleração da gravidade', '28'),
+(Default, 'fuvest','Física', '2016','primeira', 'termodinâmica', 'estudo dos gases', '29'),
+(Default, 'fuvest','Física', '2016','primeira', 'hidrostática', 'empuxo', '30'),
+(Default, 'fuvest','Física', '2016','primeira', 'física moderna', 'aniquilação', '31'),
+(Default, 'fuvest','Física', '2016','primeira', 'óptica', 'refração', '32'),
+(Default, 'fuvest','Física', '2016','primeira', 'análise dimensonal', 'mecânica', '33');
+
+insert into questoes values
+(Default, 'fuvest','Física', '2017','primeira', 'mecânica', 'cinemática', '57'),
+(Default, 'fuvest','Física', '2017','primeira', 'mecânica', 'quantidade de movimento', '58'),
+(Default, 'fuvest','Física', '2017','primeira', 'mecânica', 'força resistência do ar', '59'),
+(Default, 'fuvest','Física', '2017','primeira', 'mecânica', 'força elástica', '60'),
+(Default, 'fuvest','Física', '2017','primeira', 'eletromagnetismo', 'campo magnético', '61'),
+(Default, 'fuvest','Física', '2017','primeira', 'eletrodinâmica', 'potência', '62'),
+(Default, 'fuvest','Física', '2017','primeira', 'ondulatória', 'velocidade onda', '63'),
+(Default, 'fuvest','Física', '2017','primeira', 'física moderna', 'decaimento radioativo', '64'),
+(Default, 'fuvest','Física', '2017','primeira', 'óptica', 'refração', '65'),
+(Default, 'fuvest','Física', '2017','primeira', 'termodinâmica', 'calorimetria', '66'),
+(Default, 'fuvest','Física', '2017','primeira', 'eletrostática', 'carga elétrica', '67'),
+(Default, 'fuvest','Física', '2017','primeira', 'física moderna', 'fótons', '68');
+
+insert into questoes values
+(Default, 'fuvest','Física', '2018','primeira', 'termodinâmica', 'calorimetria', '1'),
+(Default, 'fuvest','Física', '2018','primeira', 'óptica', 'lentes', '2'),
+(Default, 'fuvest','Física', '2018','primeira', 'termodinâmica', 'calorimetria', '3'),
+(Default, 'fuvest','Física', '2018','primeira', 'mecânica', 'queda dos corpos', '4'),
+(Default, 'fuvest','Física', '2018','primeira', 'ondulatória', 'comprimento de onda', '5'),
+(Default, 'fuvest','Física', '2018','primeira', 'mecânica', 'quantidade de movimento', '6'),
+(Default, 'fuvest','Física', '2018','primeira', 'mecânica', 'equilíbrio', '7'),
+(Default, 'fuvest','Física', '2018','primeira', 'física moderna', 'decaimento radioativo', '8'),
+(Default, 'fuvest','Física', '2018','primeira', 'eletrostática', 'ddp', '9'),
+(Default, 'fuvest','Física', '2018','primeira', 'eletrodinâmica', 'circuito', '10'),
+(Default, 'fuvest','Física', '2018','primeira', 'eletrodinâmica', 'potência', '11');
+
+insert into questoes values
+(Default, 'fuvest','Física', '2019','primeira', 'mecânica', 'pressão', '13'),
+(Default, 'fuvest','Física', '2019','primeira', 'termodinâmica', 'energia térmica', '14'),
+(Default, 'fuvest','Física', '2019','primeira', 'mecânica', 'velocidade angular', '15'),
+(Default, 'fuvest','Física', '2019','primeira', 'mecânica', 'energia mecânica', '16'),
+(Default, 'fuvest','Física', '2019','primeira', 'mecânica', 'quantidade de movimento', '17'),
+(Default, 'fuvest','Física', '2019','primeira', 'eletrostática', 'força elétrica', '18'),
+(Default, 'fuvest','Física', '2019','primeira', 'óptica', 'lentes', '19'),
+(Default, 'fuvest','Física', '2019','primeira', 'eletrodinâmica', 'circuito', '20'),
+(Default, 'fuvest','Física', '2019','primeira', 'termodinâmica', 'calorimetria', '21'),
+(Default, 'fuvest','Física', '2019','primeira', 'termodinâmica', '1a lei', '22'),
+(Default, 'fuvest','Física', '2019','primeira', 'óptica', 'lentes', '23'),
+(Default, 'fuvest','Física', '2019','primeira', 'termodinâmica', 'calorimetria', '24');
+
+insert into questoes values
+(Default, 'fuvest','Física', '2020','primeira', 'gravitação', 'órbita', '69'),
+(Default, 'fuvest','Física', '2020','primeira', 'análise dimensional', 'física moderna', '70'),
+(Default, 'fuvest','Física', '2020','primeira', 'ondulatória', 'frequência', '71'),
+(Default, 'fuvest','Física', '2020','primeira', 'ondulatória', 'velocidade', '72'),
+(Default, 'fuvest','Física', '2020','primeira', 'gravitação', 'interdisciplinar', '73'),
+(Default, 'fuvest','Física', '2020','primeira', 'mecânica', 'força elástica', '74'),
+(Default, 'fuvest','Física', '2020','primeira', 'eletrodinâmica', 'circuitos', '75'),
+(Default, 'fuvest','Física', '2020','primeira', 'óptica', 'telescópio', '76'),
+(Default, 'fuvest','Física', '2020','primeira', 'eletromagnetismo', 'força magnética', '77'),
+(Default, 'fuvest','Física', '2020','primeira', 'mecânica', 'lançamento horizontal', '78'),
+(Default, 'fuvest','Física', '2020','primeira', 'ondulatória', 'pêndulo', '79'),
+(Default, 'fuvest','Física', '2020','primeira', 'termodinâmica', 'transmissão de calor', '80');
+
+select * from questoes;
+
+insert into questoes values
+(Default, 'unicamp','Física', '2020','primeira', 'mecânica', 'cinemática', '70'),
+(Default, 'unicamp','Física', '2020','primeira', 'mecânica', 'energia cinética', '71'),
+(Default, 'unicamp','Física', '2020','primeira', 'mecânica', 'quantidade de movimento', '72'),
+(Default, 'unicamp','Física', '2020','primeira', 'mecânica', 'aceleração centrípeta', '73'),
+(Default, 'unicamp','Física', '2020','primeira', 'mecânica', 'equilíbrio', '74'),
+(Default, 'unicamp','Física', '2020','primeira', 'termodinâmica', 'lei Clayperon', '75'),
+(Default, 'unicamp','Física', '2020','primeira', 'mecânica', 'queda dos corpos', '76'),
+(Default, 'unicamp','Física', '2020','primeira', 'ondulatória', 'frequência', '77'),
+(Default, 'unicamp','Física', '2020','primeira', 'óptica', 'lentes', '78'),
+(Default, 'unicamp','Física', '2020','primeira', 'eletrodinâmica', 'circuitos', '79');
+
+insert into questoes values
+(Default, 'unicamp','Física', '2019','primeira', 'mecânica', 'cinemática', '38'),
+(Default, 'unicamp','Física', '2019','primeira', 'mecânica', 'empuxo', '39'),
+(Default, 'unicamp','Física', '2019','primeira', 'eletrodinâmica', 'corrente elétrica', '40'),
+(Default, 'unicamp','Física', '2019','primeira', 'mecanica', 'hidrostática', '41'),
+(Default, 'unicamp','Física', '2019','primeira', 'mecanica', 'lei de Fourier', '42'),
+(Default, 'unicamp','Física', '2019','primeira', 'ondulatória', 'energia', '43'),
+(Default, 'unicamp','Física', '2019','primeira', 'termodinâmica', 'calorimetria', '44'),
+(Default, 'unicamp','Física', '2019','primeira', 'ondulatória', 'frequência', '45'),
+(Default, 'unicamp','Física', '2019','primeira', 'óptica', 'visão', '46');
+
+insert into questoes values
+(Default, 'unicamp','Física', '2018','primeira', 'mecânica', 'leis de Newton', '38'),
+(Default, 'unicamp','Física', '2018','primeira', 'mecânica', 'alavanca', '39'),
+(Default, 'unicamp','Física', '2018','primeira', 'gravitação', 'órbita', '40'),
+(Default, 'unicamp','Física', '2018','primeira', 'gravitação', 'aceleração', '41'),
+(Default, 'unicamp','Física', '2018','primeira', 'mecanica', 'trabalho', '42'),
+(Default, 'unicamp','Física', '2018','primeira', 'mecânica', 'pressão', '43'),
+(Default, 'unicamp','Física', '2018','primeira', 'eletrodinâmica', 'potência', '44'),
+(Default, 'unicamp','Física', '2018','primeira', 'termodinâmica', 'calorimetria', '45'),
+(Default, 'unicamp','Física', '2018','primeira', 'eletrodinâmica', 'circuito', '46'),
+(Default, 'unicamp','Física', '2018','primeira', 'termodinâmica', 'calorimetria', '47'),
+(Default, 'unicamp','Física', '2018','primeira', 'óptica', 'lentes', '48');
+
+select * from questoes;
+
+insert into questoes values
+(Default, 'unicamp','Física', '2017','primeira', 'mecânica', 'cinemática', '57'),
+(Default, 'unicamp','Física', '2017','primeira', 'mecânica', 'cinemática', '58'),
+(Default, 'unicamp','Física', '2017','primeira', 'mecânica', 'estática', '59'),
+(Default, 'unicamp','Física', '2017','primeira', 'mecânica', 'energia', '60'),
+(Default, 'unicamp','Física', '2017','primeira', 'termodinâmica', 'estudo dos gases', '61'),
+(Default, 'unicamp','Física', '2017','primeira', 'mecânica', 'hidrostática', '62'),
+(Default, 'unicamp','Física', '2017','primeira', 'eletrodinâmica', 'corrente elétrica', '63'),
+(Default, 'unicamp','Física', '2017','primeira', 'ondulatória', 'frequência', '64'),
+(Default, 'unicamp','Física', '2017','primeira', 'óptica', 'espelho esférico', '65');
+
+insert into questoes values
+(Default, 'unicamp','Física', '2016','primeira', 'mecânica', 'cinemática', '1'),
+(Default, 'unicamp','Física', '2016','primeira', 'mecânica', 'cinemática', '2'),
+(Default, 'unicamp','Física', '2016','primeira', 'termodinâmica', 'transferência de calor', '3'),
+(Default, 'unicamp','Física', '2016','primeira', 'mecânica', 'trabalho', '4'),
+(Default, 'unicamp','Física', '2016','primeira', 'óptica', 'propriedades', '5'),
+(Default, 'unicamp','Física', '2016','primeira', 'mecânica', 'quantidade de movimento', '6'),
+(Default, 'unicamp','Física', '2016','primeira', 'eletrodinâmica', 'resitência', '7'),
+(Default, 'unicamp','Física', '2016','primeira', 'ondulatória', 'frequência', '8'),
+(Default, 'unicamp','Física', '2016','primeira', 'mecânica', 'rotação', '9'),
+(Default, 'unicamp','Física', '2016','primeira', 'mecânica', 'quantidade de movimento', '10');
